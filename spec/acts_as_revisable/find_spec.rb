98394feb6da7cda9e755d14c43e533ce0bced917
Marcelo De Polli <marcelo.polli@gmail.com>
2016-11-18 20:12:40 -0200

    Fix deprecations

--- spec/acts_as_revisable/find_spec.rb
+++ spec/acts_as_revisable/find_spec.rb
@@ -7,28 +7,28 @@ describe WithoutScope::ActsAsRevisable do
 
   describe "with a single revision" do
     before(:each) do
-      @project1 = Project.create(:name => "Rich", :notes => "a note")
+      @project1 = Project.create(name: "Rich", notes: "a note")
       @project1.update_attribute(:name, "Sam")
     end
 
     it "should just find the current revision by default" do
-      Project.find(:first).name.should == "Sam"
+      Project.first.name.should == "Sam"
     end
 
     it "should accept the with_revisions chain method" do
-      lambda { Project.scoped.with_revisions }.should_not raise_error
+      lambda { Project.all.with_revisions }.should_not raise_error
     end
 
     it "should find current and revisions with the with_revisions chain method" do
-      Project.scoped.with_revisions.size.should == 2
+      Project.all.with_revisions.size.should == 2
     end
 
     it "should find revisions with conditions" do
-      Project.where(:name => "Rich").with_revisions.should == [@project1.find_revision(:previous)]
+      Project.where(name: "Rich").with_revisions.should == [@project1.find_revision(:previous)]
     end
 
-		it "should find last revision" do
-			@project1.find_revision(:last).should == @project1.find_revision(:previous)
-		end
+    it "should find last revision" do
+      @project1.find_revision(:last).should == @project1.find_revision(:previous)
+    end
   end
 end
