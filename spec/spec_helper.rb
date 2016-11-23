c1fde9f24275c53ab4f0c4e2266fbe18330be564
Marcelo De Polli <marcelo.polli@gmail.com>
2016-11-23 18:37:38 -0200

    Add null: false option to t.timestamps

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,7 @@ def setup_db
       t.boolean :revisable_is_current
       t.integer :revisable_original_id, :revisable_branched_from_id, :revisable_number, :project_id
       t.datetime :revisable_current_at, :revisable_revised_at, :revisable_deleted_at
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table :projects do |t|
@@ -38,7 +38,7 @@ def setup_db
       t.boolean :revisable_is_current
       t.integer :revisable_original_id, :revisable_branched_from_id, :revisable_number
       t.datetime :revisable_current_at, :revisable_revised_at, :revisable_deleted_at
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table :foos do |t|
@@ -47,7 +47,7 @@ def setup_db
       t.boolean :revisable_is_current
       t.integer :revisable_original_id, :revisable_branched_from_id, :revisable_number, :project_id
       t.datetime :revisable_current_at, :revisable_revised_at, :revisable_deleted_at
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table :posts do |t|
@@ -55,7 +55,7 @@ def setup_db
       t.boolean :revisable_is_current
       t.integer :revisable_original_id, :revisable_branched_from_id, :revisable_number
       t.datetime :revisable_current_at, :revisable_revised_at, :revisable_deleted_at
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
